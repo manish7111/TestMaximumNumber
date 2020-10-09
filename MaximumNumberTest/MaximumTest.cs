0bc65d5082d8e7414f7a2c3e80f308cc52eb3bed
mmkr7111 <mmkr7111@gmail.com>
2020-10-09 20:16:08 +0530

    [Manish Reddy]Add : Implemented Maximum method which returns maximum float number among 3 positions

--- MaximumNumberTest/MaximumTest.cs
+++ MaximumNumberTest/MaximumTest.cs
@@ -34,5 +34,34 @@ namespace MaximumNumberTest
             int actualMax = MaximumNumberCheck.MaximumIntegerNumber(225, 375, 467);
             Assert.AreEqual(467, actualMax);
         }
+        /// <summary>
+        /// Test case 2.1
+        /// </summary>
+        [TestMethod]
+        public void GivenFloatNumFirstPostion_whenFindMax_shouldReturnsMaxFirst()
+        {
+            double actualMax = MaximumNumberCheck.MaximumFloatNumber(55.5, 40.5, 20.5);
+            Assert.AreEqual(55.5, actualMax);
+        }
+
+        /// <summary>
+        /// Test case 2.2
+        /// </summary>
+        [TestMethod]
+        public void GivenFloatNumSecondPostion_whenFindMax_shouldReturnsMaxSecondPosition()
+        {
+            double actualMax = MaximumNumberCheck.MaximumFloatNumber(40.5, 55.5, 20.5);
+            Assert.AreEqual(55.5, actualMax);
+        }
+
+        /// <summary>
+        /// Test case 2.3
+        /// </summary>
+        [TestMethod]
+        public void GivenFloatNumThirdPostion_whenFindMax_shouldReturnsMaxThirdPosition()
+        {
+            double actualMax = MaximumNumberCheck.MaximumFloatNumber(40.5, 20.5, 55.5);
+            Assert.AreEqual(55.5, actualMax);
+        }
     }
 }
